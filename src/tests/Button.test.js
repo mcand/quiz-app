0719a57a4619f642e2d129ea0be08ddb2afe2e4b
Andre Furquim <=>
2020-12-21 00:14:10 -0300

    :wrench: Add jest setup file

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Button from '../Button';
-
-configure({ adapter: new Adapter() });
 
 describe('Button', () => {
   it('renders a <button>', () => {
