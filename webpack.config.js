f4c2cb638bd3e48895ae08093a1c9e6d1f4ca5ad
Andre Furquim <=>
2020-12-21 01:32:17 -0300

    :bulb: Add example page

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry:{
+    app: './src/App.js',
+    example: './example/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Quiz-App',
+      chunks: ['example'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: require.resolve('babel-loader'),
+      },
+    ],
+  },
+};
