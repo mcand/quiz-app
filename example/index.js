96440d6678f8818b703283c38380a585e00ad75e
Andre Furquim <=>
2020-12-21 01:32:17 -0300

    :bulb: Add example page

---
+++ example/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/App';
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<App />, container);
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return (
+    <Button>Test</Button>
+  )
+}
+
+export default App;
